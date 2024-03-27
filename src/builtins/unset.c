242a0e00aa5240fb194e84907565029fd8ffd884
trysinsk <rico_co95@hotmail.fr>
2024-03-27 10:20:51 +0100

    update unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: trysinsk <trysinsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:37:31 by lannur-s          #+#    #+#             */
-/*   Updated: 2024/03/15 10:09:30 by lannur-s         ###   ########.fr       */
+/*   Updated: 2024/03/27 10:20:09 by trysinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,35 @@ int	builtin_unset(t_execcmd_P *ecmd, t_core_struct *core)
 {
 	t_env_list  *current;
 	t_env_list  *previous;
+	int	i;
 
-	current = core->env_list;
-	previous = current;
-	if ((ecmd->argv[1] == NULL) || (ecmd->argv[1][0] == '\0'))
-		return (1);	
-	while (ft_strcmp(current->name, ecmd->argv[1]) != 0)
+	i = 1;
+	while (ecmd-> argv[i] != NULL)
 	{
+		current = core->env_list;
 		previous = current;
-		current = current->next;
-	}
-	if (ft_strcmp(current->name, core->env_list->name) == 0)
-	{
-		core->env_list = core->env_list->next;
-		free(current->name);
-		free(current->value);
-		current->next = NULL;
-	}
-	else if (current != NULL)
-	{
-		previous->next = current->next;
-		free(current->name);
-		free(current->value);
-		current->next = NULL;
+		if ((ecmd->argv[i] == NULL) || (ecmd->argv[i][0] == '\0'))
+			return (1);	
+		while (current != NULL && ft_strcmp(current->name, ecmd->argv[i]) != 0)
+		{
+			previous = current;
+			current = current->next;
+		}
+		if (current != NULL && ft_strcmp(current->name, core->env_list->name) == 0)
+		{
+			core->env_list = core->env_list->next;
+			free(current->name);
+			free(current->value);
+			current->next = NULL;
+		}
+		else if (current != NULL)
+		{
+			previous->next = current->next;
+			free(current->name);
+			free(current->value);
+			current->next = NULL;
+		}
+		i++;
 	}
 	return (0);
 }
