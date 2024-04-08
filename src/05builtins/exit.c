a94bcfb48bbe455553cccc9132e5879fd222e6eb
lannur-s <lathaannur@gmail.com>
2024-04-08 15:21:12 +0200

    FIX: EXIT

--- src/05builtins/exit.c
+++ src/05builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: trysinsk <trysinsk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:37:31 by lannur-s          #+#    #+#             */
-/*   Updated: 2024/04/08 11:33:22 by trysinsk         ###   ########.fr       */
+/*   Updated: 2024/04/08 15:14:47 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	builtin_exit(t_execcmd_P *ecmd, t_core_struct *core, char *prompt)
 	{
 		ft_printf (" numeric argument required\n");
 		core->exit_code = 2;
-		exit (core->exit_code);
+		return core->exit_code;
 	}
 	if (ecmd->argv[1] != NULL && ecmd->argv[2] != NULL)
 	{
 		ft_printf (" too many arguments\n");
 		core->exit_code = 1;
-		exit (core->exit_code);
+		return core->exit_code;
 	}
 	if (ecmd->argv[1] != NULL)
 		core->exit_code = ft_atoi(ecmd->argv[1]);
